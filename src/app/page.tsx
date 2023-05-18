2449728be03b121353f43bd9bd44705d27d78816
Simon Gribert <simon.gribert@live.se>
2023-05-18 16:59:45 +0200

    [Style] Use SVH instead of vh to adjust size for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import Carousel from '@/components/Carousel'
 
 const Home = () => {
   return (
-    <main className="h-screen w-full overflow-hidden flex justify-center items-center p-4 bg-slate-300 dark:bg-slate-950">
+    <main className="w-full overflow-hidden flex justify-center items-center p-4 bg-slate-300 dark:bg-slate-950"
+    style={{
+      height: '100svh'
+    }}>
       <Carousel />
     </main>
   )
